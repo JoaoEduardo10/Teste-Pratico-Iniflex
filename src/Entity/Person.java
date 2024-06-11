c2c5af3199878ad2b93a6717ca53851d9080d8b8
João Eduardo <joaoeduardoj920@gmail.com>
2024-06-11 20:43:34 -0300

    feat(entity): creating person entity

---
+++ src/Entity/Person.java
@@ -0,0 +1,42 @@
+package Entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Person {
+    private  String name;
+    private LocalDate age;
+
+    public Person(String name, LocalDate age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getAge() {
+        return age;
+    }
+
+    public void setAge(LocalDate age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return Objects.equals(name, person.name) && Objects.equals(age, person.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
